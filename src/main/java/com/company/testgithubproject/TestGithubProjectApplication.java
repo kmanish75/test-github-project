0d9b5788d52c5291886094786579c44c55882ad5
Manish <kmanish75@gmail.com>
2019-08-15 09:34:17 -0500

    first commit

---
+++ src/main/java/com/company/testgithubproject/TestGithubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.testgithubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubProjectApplication.class, args);
+	}
+
+}
